43c20fb6b9bd20c7671d4664737a405b08c05a80
Sanjai <developer.sanjai@gmail.com>
2023-01-23 18:50:25 +0530

    functions initial commit

--- Function with inputs/main.py
+++ Function with inputs/main.py
@@ -6,7 +6,15 @@ def greet(name):
     print(f"How are you?{name}")
     print(f"Have a nice day!{name}")
 
+run = True
 
-user_name = input("Enter your name: ")
+while run:
+    user_name = input("Enter your name: ")
 
-greet(name = user_name)
+    greet(name = user_name)
+
+    user_choice = input("Do you want to continue?...(Y/N) ").lower()
+
+    if user_choice == "n" :
+        run = False
+    
